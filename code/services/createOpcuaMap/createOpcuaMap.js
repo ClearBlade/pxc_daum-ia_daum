6ec3ffcc720609e6f8cf2db5244be082b497cfb0
Jim Bouquet <jbouquet@clearblade.com>
2025-02-20 14:52:45 -0600

    Change code from typescript to javascript

--- code/services/createOpcuaMap/createOpcuaMap.js
+++ code/services/createOpcuaMap/createOpcuaMap.js
@@ -1,4 +1,31 @@
-!function(){var e={};e.g=function(){if("object"==typeof globalThis)return globalThis;try{return this||new Function("return this")()}catch(e){if("object"==typeof window)return window}}(),e.g.createOpcuaMap=function(e,t){var n=e.params.edge_id,r=e.params.mappings;ClearBladeAsync.Collection("tag_attribute_mapping").upsert({edge_id:n,mappings:r},"edge_id").then((function(e){return t.success(e)})).catch((function(e){
-    console.log(e);
-    return t.error(e.message)
-    }))}}();
+/**
+ * Type: Micro Service
+ * Description: A short-lived service which is expected to complete within a fixed period of time.
+ * @param {CbServer.BasicReq} req
+ * @param {string} req.systemKey
+ * @param {string} req.systemSecret
+ * @param {string} req.userEmail
+ * @param {string} req.userid
+ * @param {string} req.userToken
+ * @param {boolean} req.isLogging
+ * @param {[id: string]} req.params
+ * @param {CbServer.Resp} resp
+ */
+function createOpcuaMap(req, resp) {
+  const edge_id = req.params.edge_id;
+  const mappings = req.params.mappings;
+
+  const collection = ClearBladeAsync.Collection('tag_attribute_mapping');
+  collection
+    .upsert(
+      { 
+        "edge_id": edge_id, 
+        "mappings": mappings
+      }, 'edge_id')
+    .then(function (data) {
+      resp.success(data);
+    })
+    .catch(function (err) {
+      resp.error(err.message)
+    });
+}
